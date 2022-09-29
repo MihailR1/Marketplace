dac14842e5df07e1ac7a215b3cb88f977f3b0642
MihailR1 <112471585+MihailR1@users.noreply.github.com>
2022-09-29 14:26:00 +0500

    Categories Tree (#2)
    * Categories Tree

--- webapp/model.py
+++ webapp/model.py
@@ -1,6 +1,8 @@
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy_mptt.mixins import BaseNestedSets
 
 db = SQLAlchemy()
 
@@ -25,3 +27,31 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f'<User {self.id}>'
+
+
+class Category(db.Model, BaseNestedSets):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), index=True, unique=True)
+
+    def __repr__(self):
+        return f'<Category {self.name}>'
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category_id = db.Column(db.Integer, db.ForeignKey(Category.id))
+    category = relationship('Category', backref='products')
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    user = relationship('User', backref='products')
+    name = db.Column(db.String(75), index=True, unique=True, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    photos_path = db.Column(db.String, nullable=False)  # Путь до всех фото каталога
+    description = db.Column(db.Text)
+    brand_name = db.Column(db.String(40))
+    color = db.Column(db.String(20))
+    gender = db.Column(db.String(7))
+    size = db.Column(db.String(10))
+
+    def __repr__(self):
+        return f'<Product name {self.name}, id {self.id}, category {self.category}>'
