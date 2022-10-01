cb1017e758c22a6f81d2925b55573e09fe7169b1
MihailR <MihailR.20@yandex.ru>
2022-10-01 12:01:12 +0500

    Правки + добавил ссылку на карточку товара

--- save_products_in_db.py
+++ save_products_in_db.py
@@ -7,32 +7,42 @@ from webapp import create_app
 from webapp.db import db
 from webapp.marketplace.models import Product, Category
 
-with open('products.json', 'r', encoding='utf-8') as file:
-    PRODUCT_JSON = json.load(file)
-
 app = create_app()
 
+
+def products_to_db(id, name, price, description):
+    product_exist = Product.query.filter(Product.id == id).count()
+
+    if not product_exist:
+        product_handler = Product(name=name, user_id=1,
+                                  price=price, description=description, photos_path='../../static/images/0.png',
+                                  category_id=2, color='Натуральный', size='Натуральный')
+
+        db.session.add(product_handler)
+        db.session.commit()
+
+
+def save_products(json_file):
+    # Создание категории и подкатегории в БД
+    db.session.add(Category(name="Продкуты"))
+    db.session.add(Category(name="Фрукты", parent_id=1))
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+    # Чтение json файла и сохранение в переменную
+    with open(json_file, 'r', encoding='utf-8') as file:
+        products_json = json.load(file)
+
+    for product in products_json:
+        item = products_json[product]
+        name = item['name']
+        price = item['cost']
+        id = product
+        description = item['description']
+
+        products_to_db(id, name, price, description)
+
+
 with app.app_context():
-    def save_products(id, name, price, description, img):
-        product_exist = Product.query.filter(Product.id == id).count()
-        if not product_exist:
-            product_handler = Product(id=id, name=name, user_id=1,
-                                      price=price, description=description, photos_path=img,
-                                      category_id=1, color='Натуральный', size='Натуральный')
-            db.session.add(product_handler)
-            db.session.commit()
-
-
-    def get_products(json_file):
-        for product in json_file:
-            item = json_file[product]
-            id = product
-            name = item['name']
-            price = item['cost']
-            description = item['description']
-            img_path = item['image']
-
-            save_products(id, name, price, description, img_path)
-
-
-    get_products(PRODUCT_JSON)
+    save_products('products.json')
--- webapp/marketplace/views.py
+++ webapp/marketplace/views.py
@@ -1,11 +1,22 @@
-from flask import Blueprint, render_template
+from flask import abort, Blueprint, render_template
 
 from webapp.marketplace.models import Product
 
 blueprint = Blueprint('marketplace', __name__)
 
+
 @blueprint.route('/')
 def index():
     title = "Каталог товаров"
-    get_all_products = Product.query.all()
-    return render_template('marketplace/index.html', page_title=title, products_list=get_all_products)
+    products = Product.query.all()
+    return render_template('marketplace/index.html', page_title=title, products=products)
+
+
+@blueprint.route('/product/<int:product_id>')
+def product_page(product_id):
+    product = Product.query.filter(Product.id == product_id).first()
+
+    if not product:
+        abort(404)
+
+    return render_template('marketplace/product_page.html', page_title='Карточка товара', product=product)
