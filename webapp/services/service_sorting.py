cbb8230ffb8aec8fc6dcf5f5331dc9363dc2318c
MihailR <MihailR.20@yandex.ru>
2022-11-25 13:50:30 +0500

    fix sorting func

--- webapp/services/service_sorting.py
+++ webapp/services/service_sorting.py
@@ -3,12 +3,11 @@ from webapp.marketplace.models import Product
 
 
 def process_sorting_product_types(user_sorting_type):
-    
     user_sorting_type = ProductSortingTypes(user_sorting_type)
-    
+
     if user_sorting_type == ProductSortingTypes.price_from_min_to_max:
         products = Product.query.order_by(Product.price).limit(20)
     elif user_sorting_type == ProductSortingTypes.price_from_max_to_min:
         products = Product.query.order_by(Product.price.desc()).limit(20)
-    
+
     return products
